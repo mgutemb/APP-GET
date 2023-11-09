715203bce2f220d16a14577153d3fd2d3d94b90b
Millena Gutemberg <imlira90@gmail.com>
2023-11-08 22:17:17 -0300

    update files

--- get.js
+++ get.js
@@ -74,9 +74,9 @@ function calcular() {
 }
 
 function limpar() {
-    document.getElementById("imc").innerHTML = ""
-    document.getElementById("status").innerHTML = ""
+    document.getElementById("imc").innerHTML = "IMC"
+    document.getElementById("status").innerHTML = "status"
     document.getElementById("freq").innerHTML = ""
     document.getElementById("grafico").src = "icons/reset.png"
-    document.getElementById("calorias").innerHTML = ""
+    document.getElementById("calorias").innerHTML = "Calorias"
 }
